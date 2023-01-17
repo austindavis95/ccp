780cd563960607aa1c03e8cc6e6d284da0d6d334
Austin Davis <austindavis@Austins-MBP.attlocal.net>
2023-01-16 23:39:44 -0500

    setup components

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,33 @@
-import React from 'react'
+import styles from './style';
 
-const App = () => {
-  return (
-    <div>App</div>
-  )
-}
+const App = () => (
+    <div className="bg-primary w-full overflow-hidden">
+     <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+      <div className={`${styles.boxWidth}`}>
+        Navbar
+      </div>
+     </div>
+
+     <div className={`bg-primary ${styles.flexStart}`}>
+       <div className={`${styles.boxWidth}`}>
+         hero
+       </div>
+     </div>
+
+     <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+       <div className={`${styles.boxWidth}`}>
+        shows
+        listen
+        merch
+        socials
+        footer
+
+       </div>
+     </div>
+
+
+    </div>
+  );
+
 
 export default App
---
+++ src/components/listen.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const listen = () => {
+  return (
+    <div>listen</div>
+  )
+}
+
+export default listen
